92f0c34583b68e200fc33beec50390118620c54d
airoofs.roman@gmail.com <airoofs.roman@gmail.com>
2024-01-09 08:02:01 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+from anvil.js.window import Appwrite
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -7,3 +8,4 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    
